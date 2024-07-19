95ffe5cb3682860e0293f5570747a5a443419c57
MEHDIJAD <elmehdielgarouaz@gmail.com>
2024-07-19 10:15:31 +0100

    ex05 pointer

---
+++ days/C01/ex05_ft_putstr.c
@@ -0,0 +1,29 @@
+#include<unistd.h>
+
+void ft_putstr(char *str)
+{
+    while(*str)
+    {
+        write(1, str, 1);
+        ++str;
+    }
+    
+
+}
+
+
+int main()
+{
+    char c[] = "hello";
+    ft_putstr(c);
+
+    
+    
+
+
+
+}
+
+
+    
+
